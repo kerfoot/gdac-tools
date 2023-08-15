110cb7008c88898f161d5c7d5c83e874aedc2347
John Kerfoot <johnkerfoot@gmail.com>
2023-08-15 10:31:31 -0400

    updated arg processing

--- examples/load_erddap_client.py
+++ examples/load_erddap_client.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from dateutil import parser
 from gdt.erddap import GdacClient
+from gdt.erddap.osmc import OsmcClient
 from gdt.plotting.calendars import plot_calendar
 from pprint import pprint as pp
 
@@ -28,58 +29,63 @@ response = 'csv'
 pretty_print = 'psql'
 include_summaries = False
 debug = True
-hours = 72
-start_ts = '2023-08-02T00:00:00'
+hours = 24
+start_ts = None
 end_ts = None
 north = 90
 south = -90
 east = 180
 west = -180
 search_string = ''
-variable_name = ''
+variable_names = ''
 glider = None
 params = {}
 
 client = GdacClient()
 
+# Use the min and max times from allDatasets
 start_time = client.erddap_datasets.mintime.min()
 end_time = client.erddap_datasets.maxtime.max()
 
+params = {}
 if not dataset_ids:
 
-    if hours:
+    if start_ts:
+        hours = 0
+        try:
+            start_time = parser.parse(start_ts)
+            logging.info('User specified start_time: {:}'.format(start_time.strftime('%Y-%m-%dT%H:%M:%S')))
+        except Exception as e:
+            logging.error('User specified start_time: {:} ({:})'.format(start_ts, e))
+
+    if end_ts:
+        hours = 0
+        try:
+            end_time = parser.parse(end_ts)
+            logging.info('User specified end_time: {:}'.format(end_time.strftime('%Y-%m-%dT%H:%M:%S')))
+        except Exception as e:
+            logging.error('User specified end_time: {:} ({:})'.format(end_ts, e))
+
+    if hours > 0:
         logging.info('Searching for data sets updated within the last {:} hours'.format(hours))
         start_time = datetime.datetime.utcnow() - datetime.timedelta(hours=hours)
-    else:
-        if start_ts:
-            try:
-                start_time = parser.parse(start_ts)
-                logging.info('User specified start_time: {:}'.format(start_time.strftime('%Y-%m-%dT%H:%M:%S')))
-            except Exception as e:
-                logging.error('User specified start_time: {:} ({:})'.format(start_ts, e))
 
-        if end_ts:
-            try:
-                end_time = parser.parse(end_ts)
-                logging.info('User specified end_time: {:}'.format(end_time.strftime('%Y-%m-%dT%H:%M:%S')))
-            except Exception as e:
-                logging.error('User specified end_time: {:} ({:})'.format(end_ts, e))
+start_time = start_time.strftime('%Y-%m-%dT%H:%M')
+end_time = end_time.strftime('%Y-%m-%dT%H:%M')
+logging.info('Search start time: {:}'.format(start_time))
+logging.info('Search end time  : {:}'.format(end_time))
 
-        start_time = start_time.strftime('%Y-%m-%dT%H:%M')
-        end_time = end_time.strftime('%Y-%m-%dT%H:%M')
-        logging.info('Search start time: {:}'.format(start_time))
-        logging.info('Search end time  : {:}'.format(end_time))
+params = {'min_time': start_time,
+          'max_time': end_time,
+          'min_lat': south,
+          'max_lat': north,
+          'min_lon': west,
+          'max_lon': east,
+          'search_for': search_string,
+          'variableName': variable_names
+          }
 
-    params = {'min_time': start_time,
-              'max_time': end_time,
-              'min_lat': south,
-              'max_lat': north,
-              'min_lon': west,
-              'max_lon': east,
-              'search_for': search_string,
-              'variableName': variable_name
-              }
-
+# Search the ERDDAP server
 client.search_datasets(params=params, dataset_ids=dataset_ids)
 
 datasets = client.datasets
@@ -90,3 +96,4 @@ if glider:
 
 logging.info('Found {:} data sets matching search criteria'.format(datasets.shape[0]))
 
+osmc_client = OsmcClient()
